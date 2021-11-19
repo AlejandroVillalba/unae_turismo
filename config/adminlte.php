575015d9b203cd9e68bfbd7b267d9d317d64504c
Alejandro Villalba <alejandrovillalba007@gmail.com>
2021-11-19 00:35:28 -0300

    Rango de fecha en la vista home

--- config/adminlte.php
+++ config/adminlte.php
@@ -314,7 +314,7 @@ return [
             ],
         ],
         'Sweetalert2' => [
-            'active' => true,
+            'active' => false,
             'files' => [
                 [
                     'type' => 'js',
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,12 +8,37 @@
 
 @section('content')
     <p>vista de inicio</p>
+    <div class="form-group">
+        <label>Rango de fechas:</label>
+
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <span class="input-group-text">
+              <i class="far fa-calendar-alt"></i>
+            </span>
+          </div>
+          <input type="text" name="daterange" class="form-control float-right" value="11/19/2021 - 11/20/2021" />
+        </div>
+        <!-- /.input group -->
+    </div>
+    
 @stop
 
 @section('css')
- 
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
 @stop
 
 @section('js')
-    
+<script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+<script>
+    $(function() {
+      $('input[name="daterange"]').daterangepicker({
+        opens: 'left'
+      }, function(start, end, label) {
+        console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+      });
+    });
+    </script>
 @stop
