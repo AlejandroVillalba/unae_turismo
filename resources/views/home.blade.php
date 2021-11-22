8e0318b8658af3257b29d497e6eca8e1d9a7031f
Alejandro Villalba <alejandrovillalba007@gmail.com>
2021-11-21 23:49:30 -0300

    Vista Home y crud de alojamiento

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,31 +4,100 @@
 @section('title', 'Home')
 
 @section('content_header')
-    <h1>Inicio</h1>
+<div class="shadow p-3 mb-5 bg-white rounded"><h1>Inicio</h1></div>
 @stop 
 
 @section('content')
 
-  <div class="card card-outline card-primary">
-    <div class="card-header">
-      <h3 class="card-title">Rango de fechas:</h3>
-    </div>
-    <div class="form-row">
-      <div class="card-body">
-        <div class="col-3">
-          <div class="input-group">
-            <div class="input-group-prepend">
-              <span class="input-group-text">
-                <i class="far fa-calendar-alt"></i>
-              </span>
+  <div class="card bg-dark text-white">
+    <img src="http://3.bp.blogspot.com/--QsqSRaBcFE/UNqBXF_SENI/AAAAAAAABZU/kZgcV-vnnBs/s1600/2.jpg" class="card-img" alt="...">
+    <div class="card-img-overlay">
+      <h5 class=""><strong>Encontrá ofertas en hoteles, casas y mucho más</strong></h5>
+      <p class="card-text">Desde cómodas casas de campo hasta departamentos con onda en la ciudad</p>
+
+      <div class="form-row">
+
+          <div class="col-sm-3 my-1">
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text">
+                  <i class="far fa-calendar-alt"></i>
+                </span>
+              </div>
+                <input type="text" name="daterange" class="form-control " value="" />
             </div>
-              <input type="text" name="daterange" class="form-control " value="11/19/2021 - 11/20/2021" />
           </div>
-        </div>
-      </div><!-- /.card-body -->
-    </div><!-- /.form row -->
-    <div class="card-footer">
-      The footer of the card
+
+          <div class="col-sm-3 my-1">
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text">
+                  <i class="fas fa-bed"></i>
+                </span>
+              </div>
+                <select class="form-control">
+                  <option selected>Habitaciones</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
+                </select>
+            </div>
+          </div>
+
+          <div class="col-sm-3 my-1">
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text">
+                  <i class="fas fa-user-tie"></i>
+                </span>
+              </div>
+                <select class="form-control">
+                  <option selected>Adultos</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
+                </select>
+            </div>
+          </div>
+
+          <div class="col-sm-3 my-1">
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text">
+                  <i class="fas fa-baby"></i>
+                </span>
+              </div>
+                <select class="form-control">
+                  <option selected>Menores</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
+                </select>
+            </div>
+          </div>
+      </div><!-- /.form row -->
+      <button type="button" class="btn btn-primary btn-lg btn-block ">Buscar</button>
     </div>
   </div>
 
@@ -37,7 +106,10 @@
 @section('footer')
   <strong>Copyright © 2021-2021 <a href="https://www.unae.edu.py/tv/">UNAE</a>.</strong> Reservados todos los derechos.
   <div class="float-right d-none d-sm-inline">v1.0</div>
-  @unless (Auth::check()) No has iniciado sesión. <a href="{{ route('login') }}" >Iniciar Sesión</a> @endunless
+  @unless (Auth::check()) No has iniciado sesión.
+  <a href="{{ route('login') }}" >Iniciar Sesión</a>/
+  <a href="{{ route('register') }}" >Registrarse</a> 
+  @endunless
 @endsection
 
 @section('css')
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AlojamientoController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
@@ -11,3 +12,4 @@ use Illuminate\Support\Facades\Route;
 // });
 Route::get('/', [HomeController::class, 'home'])->name('home');
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::resource('/alojamientos', AlojamientoController::class);
